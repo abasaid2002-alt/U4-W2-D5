32bed2eea6f19ed2271f0f948eb90a4533a58f1c
Abanob <aba.said2002@gmail.com>
2026-03-20 10:31:40 +0100

    creazione struttura

---
+++ src/main/java/abanobsaid/collezionegiochi/Application.java
@@ -0,0 +1,8 @@
+package abanobsaid.collezionegiochi;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/abanobsaid/collezionegiochi/Gioco.java
@@ -0,0 +1,4 @@
+package abanobsaid.collezionegiochi;
+
+public class Gioco {
+}
